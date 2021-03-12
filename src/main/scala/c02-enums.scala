ea50f2aadf3e8475d19ec6ba0d9aa42a33eee1b8
wiem <wiemzin@gmail.com>
2021-03-12 14:40:05 +0100

    exercise2

--- src/main/scala/c02-enums.scala
+++ src/main/scala/c02-enums.scala
@@ -4,11 +4,6 @@
 
 // Enums in Scala 2 emulated with sealed traits/abstract classes
 // and a combination of case objects with case classes.
-sealed trait ShipmentStatus
-case object InPreparation extends ShipmentStatus
-case object Dispatched extends ShipmentStatus
-case object InTransit  extends ShipmentStatus
-case object Delivered extends ShipmentStatus
 
 /**
  * Example 1: Simple enums
@@ -16,6 +11,17 @@ case object Delivered extends ShipmentStatus
  * [] multiline enums
  * [] enum methods/companion object
  */
+enum ShipmentStatus:
+    case InPreparation, Dispatched, InTransit, Delivered
+
+object ShipmentStatus:
+    def fromString(str: String): ShipmentStatus = ???
+object EnumUsage:
+    import ShipmentStatus.*
+    ShipmentStatus.values
+    ShipmentStatus.valueOf("InPreparation")
+    ShipmentStatus.Delivered.ordinal
+    ShipmentStatus.fromString("")
 
 /**
  * Example 2: ADTs
@@ -23,12 +29,13 @@ case object Delivered extends ShipmentStatus
  * [] type widening
  * [] type params
  */
-sealed abstract class Customer(val priority: Int) 
-object Customer {
-    case class Standard(name: String) extends Customer(priority = 3)
-    case class Premium(name: String) extends Customer(priority = 1)
-    case class Business(companyName: String, vatId: String) extends Customer(priority = 2)
-}
+enum Customer(val priority: Int):
+    case Standard(name: String) extends Customer(priority = 3)
+    case Premium(name: String) extends Customer(priority = 1)
+    case Business(companyName: String, vatId: String) extends Customer(priority = 2)
+
+object example:
+    val x = new Customer.Standard("ok")
 
 /**
  * Exercises
@@ -37,33 +44,26 @@ object enumsExercises:
     //
     // Exercise 1: Convert to Scala 3 enum syntax.
     //
-    sealed trait OrderStatus {
+    enum OrderStatus:
         final def isLegalSuccessorOf(status: OrderStatus): Boolean =
             OrderStatus.legalSuccessors.getOrElse(status, Set.empty).contains(this)
-    }
-    object OrderStatus {
-        case object Initiated extends OrderStatus
-        case object Cancelled extends OrderStatus
-        case object Confirmed extends OrderStatus
-        case object Fulfilled extends OrderStatus
-        case object Refunded extends OrderStatus
-        case object Failed extends OrderStatus
-
+        case Initiated, Cancelled, Confirmed, Fulfilled, Refunded , Failed 
+    
+    object OrderStatus:
         private val legalSuccessors: Map[OrderStatus, Set[OrderStatus]] = Map(
             Initiated -> Set(Confirmed, Cancelled),
             Confirmed -> Set(Fulfilled, Failed),
             Fulfilled -> Set(Refunded, Failed)
         )
-    }
 
     //
     // Exercise 2: Implement the following ADT using the new Scala 3 enum sytanx.
     //
-    sealed abstract class PaymentAuthorizationError(retriable: Boolean)
-    case class IllegalPaymentStatus(existingPaymentId: PaymentId, existingPaymentStatus: PaymentStatus) extends PaymentAuthorizationError(retriable = false)
-    case class IllegalRequestData(reason: String) extends PaymentAuthorizationError(retriable = false)
-    case class CustomerUnknown(unknownCustomerId: CustomerId) extends PaymentAuthorizationError(retriable = false)
-    case class InvalidToken(invalidToken: Token) extends PaymentAuthorizationError(retriable = true)
+    enum PaymentAuthorizationError(retriable: Boolean):
+        case IllegalPaymentStatus(existingPaymentId: PaymentId, existingPaymentStatus: PaymentStatus) extends PaymentAuthorizationError(retriable = false)
+        case IllegalRequestData(reason: String) extends PaymentAuthorizationError(retriable = false)
+        case CustomerUnknown(unknownCustomerId: CustomerId) extends PaymentAuthorizationError(retriable = false)
+        case InvalidToken(invalidToken: Token) extends PaymentAuthorizationError(retriable = true)
 
 
     // Some type aliases to make the exercise compile... 
