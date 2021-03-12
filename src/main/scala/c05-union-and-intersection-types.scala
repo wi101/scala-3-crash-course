9ad10440a08ec543b978a05a1924537d4ea493e2
wiem <wiemzin@gmail.com>
2021-03-12 16:45:46 +0100

    exercise 5

--- src/main/scala/c05-union-and-intersection-types.scala
+++ src/main/scala/c05-union-and-intersection-types.scala
@@ -12,19 +12,30 @@ object UnionTypes:
      */
 
     // definition
-    val x: Boolean = true
+//    val x: Boolean = true
+    val x: Boolean | String | Int = 42
 
     // usage
-    def f(in: Boolean): String = ???
+    def f(in: Boolean | String | Int): String = 
+        in match
+            case _: Boolean => "bool"
+            case _: String => "string"
+            case _: Int => "int"
 
-    // How many values does type `Either[Boolean, Boolean]` have?
-    // How many values does type `Boolean | Boolean` have?
+    f(x)
+  
+     val xx: List[Int] | List[String] = ??? //  it's the same as:  List[Int | String] = ???
+    
+    // How many values does type `Either[Boolean, Boolean]` have? == 4 
+    // How many values does type `Boolean | Boolean` have? == 2 
+
+
 
     /**
      * Example 2: What type will be inferred?
      * [] Can we improve inference with union types?
      */
-    val what = if(1==1) "a" else 10
+    val what: String | Int = if 1==1 then "a" else 10
 
     /**
      * Example 3: Possible use case: union types and eithers
@@ -40,7 +51,7 @@ object UnionTypes:
     val error1: Either[FooError, String] = Left(FooError.FooError1)
     val error2: Either[BarError, String] = Left(BarError.BarError1)
 
-    val value = for 
+    val value: Either[FooError | BarError, Unit] = for 
         _ <- error1
         _ <- error2
     yield ()
@@ -49,6 +60,16 @@ object UnionTypes:
 // Exercise 1: Model a PaymentAuthorizationError ADT from Chapter 2 (enums) using a union type. 
 // What pros/cons do you see when you use a union type vs enums in modeling ADTs?
 //
+// enum PaymentAuthorizationError(retriable: Boolean):
+//        case IllegalPaymentStatus(existingPaymentId: PaymentId, existingPaymentStatus: PaymentStatus) extends PaymentAuthorizationError(retriable = false)
+//        case IllegalRequestData(reason: String) extends PaymentAuthorizationError(retriable = false)
+//        case CustomerUnknown(unknownCustomerId: CustomerId) extends PaymentAuthorizationError(retriable = false)
+//        case InvalidToken(invalidToken: Token) extends PaymentAuthorizationError(retriable = true)
+
+    case class IllegalPaymentStatus(existingPaymentId: Int, existingPaymentStatus: String, retriable: Boolean)
+    case class IllegalRequestData(reason: String, retriable: Boolean)
+    type PaymentAuthorizationError = IllegalPaymentStatus | IllegalRequestData
+
 
 object IntersectionTypes:
     /**
@@ -58,6 +79,8 @@ object IntersectionTypes:
      * [] Variance
      * [] Definition
      */
+    
+    // replaces compounds type: TypeA with TypeB with TypeC
     object Example1:
         trait A:
             def foo: String
@@ -65,8 +88,17 @@ object IntersectionTypes:
         trait B:
             def bar: Int
         
-        def x: A & B = ???
-
+        def x: A & B = new A with B {
+            override def foo: String = ???
+            override def bar: Int = ???
+        }
+        x.foo
+        x.bar    
+  
+        val a: A = x
+        val b: B = x
+        val y: List[A] & List[B] = ???
+        val yy: List[A & B] = ???
 
     /**
      * Example 2: Conflicting members
@@ -79,8 +111,12 @@ object IntersectionTypes:
         trait B:
             def foo: Int
 
-        def x: A & B = ???
-
+        def x: A & B = new A with B {
+            override def foo: String & Int = ??? // WOW!
+        }
+      
+        x.foo
+  
 
     object Example22:
         trait A:
@@ -102,3 +138,6 @@ object IntersectionTypes:
         override def f: Boolean
     trait B extends Foo:
         override def f: AnyVal
+    
+// in scala 2
+// 
